47065add8e67e8596fd6d5ce69e722b9af7cfed4
George Nicholas Padron <georgenpadron@gmail.com>
2025-06-01 01:07:44 -0400

    Updated TUI architecture

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/ebitengine/purego v0.7.1 // indirect
 	github.com/gopxl/beep v1.4.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/term v0.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,30 +2,73 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/DoozkuV/go-metronome/audio"
+	"golang.org/x/term"
 )
 
+type rawTerm struct {
+	fd       int
+	oldState *term.State
+}
+
+func CreateRawTerm() *rawTerm {
+	fd := int(os.Stdin.Fd())
+	oldState, err := term.MakeRaw(fd)
+	raw := rawTerm{fd: fd, oldState: oldState}
+	if err != nil {
+		panic(err)
+	}
+
+	// Handle Ctrl-C gracefully
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		raw.EndProgram()
+	}()
+
+	return &raw
+}
+
+func (r *rawTerm) EndProgram() {
+	term.Restore(r.fd, r.oldState)
+	fmt.Println("\nGoodbye!")
+	os.Exit(0)
+}
+
 func main() {
-	metronome := audio.NewMetronome(60)
-	metronome.Ctrl.Paused = false
+
+	// Initialize our metronome
+	m := audio.NewMetronome(60)
+	m.Ctrl.Paused = false
+
+	raw := CreateRawTerm()
+	// TODO: Find a better solution for ending the prog
+	defer raw.EndProgram()
+	// Main TUI Loop
 	for {
-		bpm := metronome.Bpm()
-		fmt.Printf("Current BPM: %v\n", bpm)
-		var input string
-		fmt.Scan(&input)
-		switch input {
-		case "+":
-			metronome.SetBpm(bpm + 2)
-		case "-":
-			metronome.SetBpm(bpm - 2)
-		default:
-			if inputNum, err := strconv.Atoi(input); err != nil {
-				fmt.Println("Bad input")
-			} else {
-				metronome.SetBpm(float64(inputNum))
-			}
+		bpm := m.Bpm()
+		// Clear line then print
+		fmt.Printf("\r+ BPM: %d - ", int(bpm))
+
+		buf := make([]byte, 1)
+		_, err := os.Stdin.Read(buf)
+		if err != nil {
+			log.Fatalf("\nError reading input: %s", err)
+		}
+
+		switch buf[0] {
+		case '=', '+':
+			m.SetBpm(bpm + 2)
+		case '-', '_':
+			m.SetBpm(bpm - 2)
+		case 'q', byte(3): // Ctrl-C
+			raw.EndProgram()
 		}
 	}
 }
